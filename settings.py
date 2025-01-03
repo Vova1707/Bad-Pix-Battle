54cc4f52fa379d1360f5143085ecd21819a36bf8
Vladimir Kondrahin <kondervova2009@gmail.com>
2025-01-03 15:35:08 +0300

    60% Итогового проекта

--- settings.py
+++ settings.py
@@ -5,15 +5,19 @@ class Settings:
         self.parent = parent
 
     def create_widgets(self):
-        self.parent.create_button((0, 500), (150, 80), 'На главную страницу', 0, lambda: self.parent.restart_surface('menu'))
-        self.parent.create_button((0, 600), (150, 80), 'Включить звук', 0, self.parent.music_on)
-        self.parent.create_button((0, 700), (150, 80), 'Выключить звук', 0, self.parent.music_off)
+        self.parent.create_button((100, 500), (600, 80), 'На главную страницу', 0, lambda: self.parent.restart_surface('menu'))
+        self.parent.create_button((100, 600), (600, 80), 'Включить звук', 0, self.parent.music_on)
+        self.parent.create_button((100, 700), (600, 80), 'Выключить звук', 0, self.parent.music_off)
 
     def listen(self):
-            self.parent.screen.fill((200, 100, 50))
-            self.parent.create_text(f'Настройки', 100, (300, 20), (0, 0, 0), (200, 100, 50))
-            self.parent.create_text(f'Имя: {self.parent.user[1]}', 24, (50, 150), (0, 0, 0), (200, 100, 50))
-            self.parent.create_text(f'Логин: {self.parent.user[2]}', 24, (50, 200), (0, 0, 0), (200, 100, 50))
-            self.parent.create_text(f'Побед: 0', 24, (50, 250), (0, 0, 0), (200, 100, 50))
-            self.parent.create_text(f'Поражения: 0', 24, (50, 300), (0, 0, 0), (200, 100, 50))
-            self.parent.create_text(f'Ничья: 0', 24, (50, 350), (0, 0, 0), (200, 100, 50))
+        self.parent.screen.blit(pygame.transform.scale(pygame.image.load(f"Images/Fon/Game_Menu.jpg"), (1600, 1000)),
+                                (0, 0))
+        self.parent.create_text(f'Настройки', 100, (300, 20), (0, 0, 0), (200, 100, 50))
+        self.parent.create_text(f'Имя: {self.parent.user[1]}', 24, (50, 150), (0, 0, 0), (200, 100, 50))
+        self.parent.create_text(f'Логин: {self.parent.user[2]}', 24, (50, 200), (0, 0, 0), (200, 100, 50))
+        self.parent.create_text(f'Побед: 0', 24, (50, 250), (0, 0, 0), (200, 100, 50))
+        self.parent.create_text(f'Поражения: 0', 24, (50, 300), (0, 0, 0), (200, 100, 50))
+        self.parent.create_text(f'Ничья: 0', 24, (50, 350), (0, 0, 0), (200, 100, 50))
+
+    def listen_event(self, event):
+        pass
