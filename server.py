4d5ce06bd4aab21182c3cd9540ebee13629c0bd5
Vladimir Kondrahin <kondervova2009@gmail.com>
2025-01-11 21:09:16 +0300

    80%

--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 import socket
 from threading import Thread
 import json
+from game_ofline import Board, Player, Enemy, Block, Object, check_delete, obj_for_kart, Block, dict_cells_group, players
+import pygame
+from game_ofline import all_sprites, groups
+import numpy as np
 
 HOST, PORT = 'localhost', 12220
 MAX_PLAYERS = 4
 
-
-SERVER_ROOM_WIDTH = 4000
-SERVER_ROOM_HEIGHT = 1000
 
 
 class Server:
@@ -15,91 +16,87 @@ class Server:
     def __init__(self, addr, max_conn):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.bind(addr)
-        self.players = []
-        self.id_players = []
         self.sock.listen(max_conn)
+
+        self.board = Board(False, 1)
+        self.id_players = []
+        self.create_other_obj()
         self.listen()
+
+    def create_other_obj(self):
+        for i in obj_for_kart[1]['object']:
+            Object(i['img'], i['pos'], i['size'], i['funct'])
 
     def listen(self):
         while True:
             try:
-                if not len(self.players) >= MAX_PLAYERS:
+                if not 1 >= MAX_PLAYERS:
                     conn, addr = self.sock.accept()
-                    print("New connection", addr)
                     Thread(target=self.handle_client,
                            args=(conn,)).start()
             except:
-                pass
+                print('sss')
+
 
     def handle_client(self, conn):
         id = list(filter(lambda f: f not in self.id_players, range(1, 5)))[0]
-
-        self.player = {
-            "id": id,
-            "x": 400,
-            "y": 300,
-            "is_moving": 1,
-            'left_right': 1,
-        }
-        
-
-        self.id_players.append(id)
-        self.players.append(self.player)
-        self.h = 0
-        self.xxx = 0
-
-        self.x = self.player["x"]
-        self.y = self.player["y"]
-        self.mov = False
+        self.main_player = Player(id)
+        self.pos = (0, 0)
         while True:
             try:
-                data = conn.recv(1024)
-                if not data:
-                    print("Disconnect")
-                    break
                 data = json.loads(data.decode('utf-8'))
-                sgp = {"get_all": lambda: self.get_all(conn),
-                       "move": lambda: self.move(data['move']),
-                       'stop': self.stop,
-                       }
-                sgp[data["request"]]()
+                if data['request'] == 'get_all':
+                    self.get_all(conn)
+                if data["request"] == 'move':
+                    print('move')
+                    self.main_player.moving_player(data['napravlenie'], data['znachenie'])
+                elif data["request"] == 'shoot':
+                    self.main_player.shoot(data['pos'])
+                elif data["request"] == 'mousedown':
+                    self.board.click(data['pos'], data['znachenie'], self.main_player)
+                elif data["request"] == 'update_index':
+                    self.main_player.update_inventory_index(data["index"])
+                elif data["request"] == 'get_cell':
+                    self.board.get_cell(data["pos"], self.main_player)
+                    self.pos = self.board.celling(data["pos"], self.main_player)
+                    print(data["pos"], 111)
+                elif data["request"] == 'click':
+                    self.board.click(data['pos'], data['funct'], self.main_player)
+                elif data["request"] == 'shoot':
+                    self.main_player.shoot(data['pos'])
+                elif data["request"] == 'choice_inventory':
+                    self.main_player.choice_inventory()
 
             except Exception as e:
-                print(e)
-                break
-        del self.id_players[self.id_players.index(id)]
-        self.players.remove(self.player)
+                print(e.__class__.__name__)
+            data = conn.recv(1024)
+            if not data:
+                pass
 
     def get_all(self, conn):
-        conn.sendall(bytes(json.dumps({"response": self.players}), 'UTF-8'))
+        for i in all_sprites: i.update()
+        for i in all_sprites[::-1]: check_delete(i)
+        for group in groups:
+            for obj in group[::-1]:
+                check_delete(obj)
+        score = 0
+        drawing = {}
 
-    def stop(self):
-        self.player['is_moving'] = 0
-        self.player["left_right"] = 0
+        for group in groups:
+            for obj in group:
+                drawing[score] = {'size': (obj.rect.w, obj.rect.h), 'pos': (obj.rect.x, obj.rect.y), 'img': obj.img_path}
+                score += 1
+        drawing['pos_player'] = self.main_player.rect.x
+        drawing['player'] = self.main_player.player
+        drawing['pos_cell_x'] = self.board.pos_cell_x
+        drawing['pos_cell_y'] = self.board.pos_cell_y
+        drawing['pos'] = self.pos
+        conn.sendall(bytes(json.dumps(drawing), 'UTF-8'))
 
-    def move(self, moving):
-        moved = {"left": lambda: self.moving("x", -10, 1),
-                 'right': lambda: self.moving("x", 10, 2),
-                 'up': lambda: self.moving("y", -10, 1),
-                 'down': lambda: self.moving("y", 10, 2),
-                 }
-        moved[moving]()
+    def close(self):
+        all_sprites.remove(self.main_player)
+        players.remove(self.main_player)
 
-    def moving(self, coord, value, l_r=False):
-        if self.player['is_moving'] == 0:
-            self.player['is_moving'] += 1
-
-        if l_r:
-            self.player["left_right"] = l_r
-        if self.xxx > 2:
-            if self.player['is_moving'] >= 3:
-                self.player['is_moving'] = 1
-            else:
-                self.player['is_moving'] += 1
-                self.xxx = 0
-        else:
-            self.xxx += 1
-        self.player[coord] += value
 
 
 if __name__ == "__main__":
